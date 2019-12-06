37cd30bec6f437d0ff9da84a31b4eb17ebd8de85
Josué Gimenes <josuegalvares@gmail.com>
2019-12-06 16:40:45 -0300

    Layouts per Page

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,22 +2,46 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
+// Layouts
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
+  // true = logado ou false = deslogado
   const signed = false;
 
+  // Se não estiver logado e rota for privada
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
   }
 
+  // Se estiver logado e rota não for privada
   if (signed && !isPrivate) {
     return <Redirect to="/dashboard" />;
   }
 
-  return <Route {...rest} component={Component} />;
+  /**
+   * Verifica se está logado e atribui o layout a variável. Dessa forma,
+   * Layout passa agora a ser um componente nesse formato <Layout />
+   */
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return (
+    <Route // component do 'react-router-dom'
+      {...rest} // Recupera todas as propriedades já existentes
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+      // <Layout> Aplica o Layout recuperado
+      // <Component {...props} /> Retorna as propriedades desse layout.
+    />
+  );
 }
 
 RouteWrapper.propTypes = {
